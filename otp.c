2d12594763c0627e0f98fb76b4c37bf8fa4f8a65
NickBarraclough <barracln@oregonstate.edu>
2020-06-05 14:44:56 -0700

    more

--- otp.c
+++ otp.c
@@ -113,7 +113,7 @@ void enc(int argc, char *argv[]){
         error("CLIENT: ERROR connecting");
     
     /* SEND THE MESSAGE LENGTH TO SERVER*/
-    for (charsWritten = 0; charsWritten < messageLength; charsWritten += send(socketFD, messageLengthChar + charsWritten, strlen(messageLengthChar) - charsWritten, 0)){
+    for (charsWritten = 0; charsWritten < messageLength; charsWritten += send(socketFD, messageLengthChar, strlen(messageLengthChar) - charsWritten, 0)){
         printf("Sent: %d bytes in the message \n", charsWritten);
     }
     
@@ -139,7 +139,7 @@ void enc(int argc, char *argv[]){
 
     /* SEND THE KEY LENGTH TO SERVER*/
 //    charsWritten = send(socketFD, keyLengthChar, strlen(keyLengthChar), 0);  // Send message to server and write to the server
-    for (charsWritten = 0; charsWritten < keyLength; charsWritten += send(socketFD, keyLengthChar + charsWritten, strlen(keyLengthChar) - charsWritten, 0)){
+    for (charsWritten = 0; charsWritten < keyLength; charsWritten += send(socketFD, keyLengthChar, strlen(keyLengthChar) - charsWritten, 0)){
         printf("Sent: %d bytes in the message \n", charsWritten);
     }
 
